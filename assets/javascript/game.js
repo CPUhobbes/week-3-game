7db7054d6016cbf42965b448a9944707ce09cbfd
Eric Steinberg <eric@MASTER.MSHOME>
2016-08-07 22:20:35 -0500

    Minor Bug Fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,13 @@
 /*
  *Global Variables
  */
-
 var soundEnabled = false;
 var enableStillScreen = false;
 var	videoDir = "assets/video/";
 var videoExt = ".mp4";
 var noAudioTag = "_noaudio";
+var audioDir = "assets/audio/";
+var audioExt = ".mp3";
 
 //filenames without extensions
 var correctGuessVideo = ["hit1", "hit2"];
@@ -15,11 +16,13 @@ var winVideo = "win";
 var loseVideo = "lose";
 var titleScreenVideo = "titleScreen";
 var startVideo = "start";
+var audioList = ["topgun1", "topgun2","topgun3","topgun4"];
 
 //Game Mechanics
 var start=true;
 var word ="";
-var dictionary = ["STABILIZER", "TURBINE", "COCKPIT", "MISSILE", "TOMCAT", "MAVERICK", "GOOSE", "ICEMAN", "VOLLEYBALL", "MOTORCYCLE", "HELICOPTER", "CARRIER"];
+var dictionary = ["STABILIZER", "TURBINE", "COCKPIT", "MISSILE", "TOMCAT", "MAVERICK", "GOOSE", "ICEMAN", 
+				  "VOLLEYBALL", "MOTORCYCLE", "HELICOPTER", "CARRIER", "BANDIT", "BOGEY"];
 var numGuesses;
 var correctGuesses;
 var guessArray;
@@ -36,12 +39,14 @@ $(document).ready(function(){
 		//Set startup video here so if video changes above no need to edit html
 		document.getElementById('videosrc').src=videoDir+titleScreenVideo+videoExt;
 		document.getElementById("Video1").load();
+		randomAudio = Math.ceil(Math.random()*4)-1;
+		changeAudio(randomAudio);
+
 });
 function sound(selection){
-	this.soundEnabled = selection;
+	soundEnabled = selection;
 	if (soundEnabled){
-		var audio = document.getElementById("audio1");
-		audio.play();
+		playAudio();
 	} 
 
 }
@@ -64,19 +69,30 @@ function changeVideo(videoFile){
 	video.play();
 
 }
+//Checks to see if video is finished
+function myHandler(e) {  
+	if(enableStillScreen){
+    	changeVideo(titleScreenVideo);
+    	if(soundEnabled){
+    		playAudio();
+		}
+	}
+    enableStillScreen = false;
+}
+function pauseAudio(){
+	var audio = document.getElementById("audio1");
+	audio.pause();
+}
+function playAudio(){
+	var audio = document.getElementById("audio1");
+	audio.play();
+}
+function changeAudio(audioFile){
+	var audio = document.getElementById("audio1");
+	var audioSource = document.getElementById('videosrc');
+	audio.src=audioDir+audioList[audioFile]+audioExt;
 
-
-    function myHandler(e) {
-    	if(enableStillScreen){
-        	changeVideo(titleScreenVideo);
-        	if(soundEnabled){
-        		var audio = document.getElementById("audio1");
-				audio.play();
-			}
-    	}
-        enableStillScreen = false;
-
-    }
+}
 
 
 /*
@@ -87,9 +103,6 @@ document.onkeyup = function(event){
 	var keyPress = String.fromCharCode(event.keyCode).toUpperCase();
 	if (keyPress && start) {
 		start=false;
-		var audio = document.getElementById("audio1");
-		audio.pause();
-		changeVideo("start");
 		startGame();
 	}
 	else{
@@ -110,8 +123,7 @@ document.onkeyup = function(event){
 					endgame(true);
 				}
 				else{
-					var audio = document.getElementById("audio1");
-					audio.pause();
+					pauseAudio();
 					changeVideo("hit"+Math.ceil(Math.random()*2));
 				}
 			}
@@ -124,8 +136,7 @@ document.onkeyup = function(event){
 					endgame(false);
 				}
 				else{
-					var audio = document.getElementById("audio1");
-					audio.pause();
+					pauseAudio();
 					changeVideo("miss"+Math.ceil(Math.random()*2));
 				}
 
@@ -138,6 +149,10 @@ document.onkeyup = function(event){
 
 
 function startGame(){
+
+	var audio = document.getElementById("audio1");
+	audio.pause();
+	changeVideo("start");
 	randomWordNum = Math.ceil(Math.random()*dictionary.length);
 	word = dictionary[randomWordNum-1];
 	numGuesses = 7;
@@ -172,16 +187,24 @@ function alreadyGuessed(letterCheck){
 function endgame(win){
 	if(win){
 		wins+=1;
-		var audio = document.getElementById("audio1");
-		audio.pause();
+		pauseAudio();
 		changeVideo("win");
+		document.getElementById("playAgain").className = "btn btn-default btn-success resetGame";
+		document.getElementById("playAgain").innerHTML ="You Won! Play Again?";
 
 	}
 	else{
 		loses+=1;
-		var audio = document.getElementById("audio1");
-		audio.pause();
+		pauseAudio();
 		changeVideo("lose");
+		document.getElementById("playAgain").className = "btn btn-default btn-danger resetGame";
+		document.getElementById("playAgain").innerHTML ="You Lost! Play Again?";
+		for(var i=0;i<word.length;++i){
+
+			document.getElementById("letterID_"+i).innerHTML=word[i];
+		}
+
+
 
 	}
 	document.getElementById("score").innerHTML="Wins: "+wins+"<br />Losses: "+loses;
